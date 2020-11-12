f2af830e256ff539c3e9bd769446380f608f930e
luckbox314 <lucas.muoz@uc.cl>
2020-11-11 22:28:02 -0300

    Placeholder images added to activities

--- FrontendISE/src/Components/Activity/ActivityItem.js
+++ FrontendISE/src/Components/Activity/ActivityItem.js
@@ -6,13 +6,13 @@ export default class ActivityItem extends React.Component {
         super(props);
         this.state = {
             activity: props.activity,
-            addActivity: props.addActivity
+            addActivity: props.addActivity,
+            image: props.activity.image,
         };
     }
-
     render() {
         return (
-            <div className={"activity-item"} id={this.state.activity.name}>
+            <div className={"activity-item"} id={this.state.activity.name} style={{backgroundImage: "url(" + this.state.activity.image + ")"}}>
                 {this.state.activity.name}
                 <div>
                     <button onClick={() => this.state.addActivity(this.state.activity)} className={'small-button'}> {this.state.activity.added?'-':'+'} </button>
--- FrontendISE/src/Components/Activity/ActivitySelect.js
+++ FrontendISE/src/Components/Activity/ActivitySelect.js
@@ -3,21 +3,24 @@ import {withRouter} from 'react-router-dom';
 import ActivityList from "./ActivityList";
 import ActivityItem from "./ActivityItem";
 import CourseListTeacherItem from "../Course/CourseListTeacherItem";
+import earth from '../../Static/images/Earth.jpg'
+
 
 class ActivitySelect extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {courseData : props.courseData || {title: "none", teacher: "none"},
+        this.state = {
+            courseData: props.courseData || {title: "none", teacher: "none"},
             activities: [
-                { name: "uno", added: false},
-                { name: "dos", added: false},
-                { name: "tres", added: false},
-                { name: "cuatro", added: false},
-                { name: "cinco", added: false},
-                { name: "seis", added: false},
-                { name: "siete", added: false},
-                { name: "ocho", added: false},
-                { name: "nueve", added: false}
+                {name: "uno", added: false, image: earth},
+                {name: "dos", added: false, image: earth},
+                {name: "tres", added: false, image: earth},
+                {name: "cuatro", added: false, image: earth},
+                {name: "cinco", added: false, image: earth},
+                {name: "seis", added: false, image: earth},
+                {name: "siete", added: false, image: earth},
+                {name: "ocho", added: false, image: earth},
+                {name: "nueve", added: false, image: earth}
             ],
             addedActivities: 0
         }
